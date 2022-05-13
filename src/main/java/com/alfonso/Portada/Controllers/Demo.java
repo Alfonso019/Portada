f6d2abcfbfa063d7583c2537490c50183bdb1e03
Ponch <Ponch@LAPTOP-PJM34VID>
2022-05-13 15:33:15 -0500

    Hola

---
+++ src/main/java/com/alfonso/Portada/Controllers/Demo.java
@@ -0,0 +1,14 @@
+package com.alfonso.Portada.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Demo {
+	
+	@GetMapping("/Portada")
+	public String Portada() {
+		return "Portada";
+	}
+
+}
